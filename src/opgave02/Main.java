edf16cfd856bd904c9c978f8d301147eb5a12943
benn-christensen <bnch@eaaa.dk>
2025-03-04 13:51:10 +0100

    added exercise 2

---
+++ src/opgave02/Main.java
@@ -0,0 +1,19 @@
+package opgave02;
+
+public class Main {
+    public static void main(String[] args) {
+        double[][] input = {
+                {1, 2, 3, 4.0},
+                {5, 6.5, 7, 8},
+                {9, 10, 11, 10},
+                {13, 14, 15, 16}
+        };
+        System.out.println(avarageMajorDiagonal(input));
+    }
+
+    private static double avarageMajorDiagonal(double[][] input) {
+        return 0;
+    }
+
+
+}
